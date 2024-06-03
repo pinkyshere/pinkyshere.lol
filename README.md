85ca0afa4ec3eeb0e3d3833db3d8df8b759c06b6
pinky <76921905+pinkyshere@users.noreply.github.com>
2024-06-02 23:04:36 -0400

    adding duh files

---
+++ README.md
@@ -0,0 +1,4 @@
+Replica of the Discord profile
+
+Discord profile data using [Lanyard API](https://github.com/Phineas/lanyard/)
+
---
+++ assets/js/config.js
@@ -0,0 +1,20 @@
+ const USER_ID = "182312095528910849"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/123.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
