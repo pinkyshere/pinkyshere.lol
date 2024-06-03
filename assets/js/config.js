7928b372000ae1f5a8be9e791abacf3df193f774
pinky <76921905+pinkyshere@users.noreply.github.com>
2024-06-03 01:50:37 -0400

    dawd

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
  const USER_ID = "182312095528910849"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
-const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
+const BACKGROUND_IMAGE = "assets/images/123.gif"; // Here is the background of the site, behind your profile
 const STATUS = {
   dnd: {
     name: "Do not disturb", // Discord Status
